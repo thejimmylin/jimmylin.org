4ceaaedaefad4e114c7dfcb224192ac81c0799af
Jimmy Lin <b00502013@gmail.com>
2022-02-19 04:36:40 +0800

    Update eslint settings.

--- components/markdown-components/PreLight.js
+++ components/markdown-components/PreLight.js
@@ -107,8 +107,10 @@ const PreLight = ({ children }) => {
             style={style}
           >
             {tokens.map((line, i) => (
+              // eslint-disable-next-line react/jsx-key
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
+                  // eslint-disable-next-line react/jsx-key
                   <span {...getTokenProps({ token, key })} />
                 ))}
               </div>
