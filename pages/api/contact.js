612f9b788c21a2ba4b28cd6db22120deeda16acf
Jimmy Lin <b00502013@gmail.com>
2022-03-08 00:15:31 +0800

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,19 +5,6 @@ const SENT_FROM = "notifications@jimmylin.org";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendMail = (msg) => {
-  return new Promise((resolve, reject) => {
-    sgMail.send(msg).then(
-      (response) => {
-        resolve(response);
-      },
-      (error) => {
-        reject(error);
-      }
-    );
-  });
-};
-
 const handler = async (req, res) => {
   if (req.method == "POST") {
     const { email, message } = req.body;
@@ -29,16 +16,15 @@ const handler = async (req, res) => {
       html: `<p>${email}: ${message}<p/>`,
     };
     try {
-      await sendMail(msg);
+      await sgMail.send(msg);
       res.status(200).end();
     } catch (error) {
-      const reason = error.response ? error.response : error;
-      const errorMsgs = [`Email is not sent because of ${reason}.`];
-      res.status(400).json({ errorMsgs });
+      const message = `Email is not sent because of ${error}.`;
+      res.status(400).json({ message });
     }
   } else {
-    const errorMsgs = ["Please use `POST` method."];
-    res.status(405).json({ errorMsgs });
+    const message = "Please use POST method.";
+    res.status(405).json({ message });
   }
 };
 
